010277421d8d0d1760b4bcabeb3cc1034e35460d
JoshuaBatty <joshpbatty@gmail.com>
2022-03-02 12:22:10 +1100

    cargo fmt

--- forc/src/ops/forc_init.rs
+++ forc/src/ops/forc_init.rs
@@ -122,7 +122,6 @@ pub(crate) fn init_new_project(project_name: String) -> Result<(), Box<dyn std::
     Ok(())
 }
 
-
 pub(crate) fn init_from_git_template(project_name: String, example_url: &Url) -> Result<()> {
     let git = parse_github_link(example_url)?;
 
